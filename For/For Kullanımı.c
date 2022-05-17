e6c647d6eae0bca62868cf781605cf3eba2db771
Yusuf Yılmaz <m_yusuf_yilmaz@hotmail.com>
2022-05-17 15:31:21 +0300

    1

---
+++ For/For Kullanımı.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    /* FOR KULLANIMI
+
+    for(Atama;condition;action)
+    {
+        statements(ifadeler-kodlar)
+    }
+
+    WHÝLE KULLANIMI
+
+    Atama;
+    while(condition)
+    {
+        statements
+        action
+    } */
+
+    int i;
+
+    for(i=0;i<=10;i++)
+        {
+            printf("%d\n",i);
+        }
+
+    /*int y=0;
+    while(y<=10)
+    {
+        printf("%d\n",y);
+        y++;
+    }
+    int a=0;
+    do
+    {
+        printf("%d\n",a);
+        a++;
+    }while(a<=10);*/
+}
---
+++ For/For example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int carpim=1;
+
+    for(i=1;i<5;i++)
+    {
+        carpim=carpim*i;
+    }
+    printf("1 den %d ye kadar olan sayilarin carpimi = %d",i,carpim);
+}
