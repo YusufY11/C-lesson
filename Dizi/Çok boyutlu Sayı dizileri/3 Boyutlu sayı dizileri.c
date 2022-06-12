0ab2774e9e605a2075441e67c05507bc8ff9a420
Yusuf Yılmaz <m_yusuf_yilmaz@hotmail.com>
2022-06-12 18:52:10 +0300

    Create 3 Boyutlu sayı dizileri.c

---
+++ Dizi/Çok boyutlu Sayı dizileri/3 Boyutlu sayı dizileri.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 3 boyutlu sayi dizisi yapma */
+
+int main()
+{
+    int ysf[2][3][4]={{{1, 5, -4, 2},{7, 8, 9, 4},{6, -1, 2, 3}},
+    {{8, 7, -3, 2},{7, 3, 2, 7},{2, -5, 2, 7}}};
+
+
+    return 0;
+}
