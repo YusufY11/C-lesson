96950809a4ba07345d06fad0d5781e63b1e22e02
Yusuf Yılmaz <m_yusuf_yilmaz@hotmail.com>
2022-06-13 22:12:20 +0300

    a

---
+++ Dizi/Karakter  dizileri/example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* \0 = Null */
+
+    char ysf[]={'y','u','s','u','f','\0'};
+
+    char dizim[]="yusuf";
+    char kullanici[0];
+
+    printf("%s\n%s\n",ysf,dizim);
+
+    printf("Lutfen bir karakter dizisi giriniz\n");
+    scanf("%s",&kullanici);
+    printf("%s",kullanici);
+
+
+    return 0;
+}
